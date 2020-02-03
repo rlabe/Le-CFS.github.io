95a6ccbeff887974288547174f46d14cd64c8dbd
rlabe <laberomain@gmail.com>
2020-02-03 14:54:29 +0100

    Optimisation des images des centres

---
+++ assets/rezise.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Purpose:
+
+# absolute path to image folder
+FOLDER=$3
+
+# max height
+WIDTH=$1
+
+# max width
+HEIGHT=$2
+
+#resize png or jpg to either height or width, keeps proportions using imagemagick
+#find ${FOLDER} -iname '*.jpg' -o -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+#resize png to either height or width, keeps proportions using imagemagick
+#find ${FOLDER} -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+#resize jpg only to either height or width, keeps proportions using imagemagick
+find ${FOLDER} -iname '*.jpg' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+# alternative
+#mogrify -path ${FOLDER} -resize ${WIDTH}x${HEIGHT}% *.png -verbose
